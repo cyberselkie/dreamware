0e563cb62dd0747aea2108fe2b7b0d122a3a1dd2
Selkie <pantywitch44@gmail.com>
2024-04-30 14:38:50 -0400

    first commit

---
+++ js/grab.js
@@ -0,0 +1,52 @@
+var draggableElements = document.getElementsByClassName("draggable");
+
+for(var i = 0; i < draggableElements.length; i++){
+    dragElement(draggableElements[i]);
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementsByClassName("draggable")) {
+            elmnt.onmousedown = dragMouseDown;  
+    } else {
+    		elmnt.onmousedown = dragMouseDown;
+    }
+    function dragMouseDown(e) {
+        e = e || window.event;
+        pos3 = parseInt(e.clientX);
+        pos4 = parseInt(e.clientY);
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+        return false;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        pos1 = pos3 - parseInt(e.clientX);
+        pos2 = pos4 - parseInt(e.clientY);
+        pos3 = parseInt(e.clientX);
+        pos4 = parseInt(e.clientY);
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+const items = document.querySelectorAll(".window");
+    items.forEach(item => {
+    item.addEventListener('mousedown', function(){
+            items.forEach(item => {
+                item.classList.remove("top");
+                this.classList.add("top");
+                if (item.classList.contains("top")) {
+                    item.querySelector(".title-bar").classList.add("active")
+                } else {
+                    item.querySelector(".title-bar").classList.remove("active")
+                }
+            })
+        })
+});
---
+++ js/toggle.js
@@ -0,0 +1,72 @@
+// show or hide a window
+function openClose(clicked_id) {
+    const btn = document.getElementById(clicked_id);
+    const targetDiv = btn.closest("#hide");
+    targetDiv.classList.add('hidden');
+};
+
+// FUCK ME FUCK YOU
+function fuck(clicked_id) {
+    const fuckMe = document.getElementById("fuckMe");
+    const fuckYou = document.getElementById("fuckYou");
+
+    if (clicked_id == "me") {
+        let btn = fuckMe.closest("#hide");
+        change(fuckMe)
+        btn.classList.remove('hidden');
+        //fuckMe.classList.toggle('container');
+    } else if (clicked_id == "you") {
+        let btn = fuckYou.closest("#hide");
+        change(fuckYou)
+        btn.classList.remove('hidden');
+        //fuckYou.classList.toggle('container');
+    };
+};
+
+function openIcon(clicked_id) {
+    if (clicked_id == "documents") {
+        const btn = document.getElementById("welcome");
+        const targetDiv = btn.closest("#hide");
+        targetDiv.classList.remove('hidden');
+    } else if (clicked_id == "outlook-icon") {
+        const btn = document.getElementById("outlook");
+        change(btn)
+        const targetDiv = btn.closest("#hide");
+        targetDiv.classList.remove('hidden');
+    } else if (clicked_id == "internet-icon") {
+        const btn = document.getElementById("internet");
+        change(btn)
+        const targetDiv = btn.closest("#hide");
+        targetDiv.classList.remove('hidden');
+    } else if (clicked_id == "chat-icon") {
+        const btn = document.getElementById("chat");
+        change(btn)
+        const targetDiv = btn.closest("#hide");
+        targetDiv.classList.remove('hidden');
+        pickCard();
+    }
+};
+
+// random location for popups
+function change(btn) {
+    var i = Math.floor(Math.random()*1000)+1;
+    var j = Math.floor(Math.random()*500)+1;
+    btn.style.left = i+"px";
+    btn.style.top = j+"px";
+}
+
+$('#start-menu-btn').change(function() {
+    $('#start-menu').toggle();
+});
+
+const helpBtn = document.getElementById('help-menu-btn')
+helpBtn.addEventListener('click', function(){
+    document.getElementById('help').closest('#hide').classList.remove('hidden');
+    $('#start-menu').toggle();
+});
+
+function message() {
+    document.getElementById("chat-box-text").innerHTML = "<moose> irc chat.sadslut.club #happy99 \n<moose> got it?";
+ }
+ setTimeout(message,180 * 1000);
+
